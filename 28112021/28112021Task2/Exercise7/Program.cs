c78f50b32fd776ff1f5be95849a4de39ab7f7fb6
royahuseynova <ryhsynv@gmail.com>
2021-11-28 21:59:01 +0400

    task2 flowcharts and other problems

---
+++ 28112021/28112021Task2/Exercise7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 dene 5 reqemli eded verilib. Bu ededleri toplayib. Alinan cavabin evveline ve axirina 5 artir. Neticenin 5 % tap
+        }
+    }
+}
