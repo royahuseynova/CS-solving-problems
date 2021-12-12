41d8abceab0f5c7e7828d6a07554ce1f64e222f0
royahuseynova <ryhsynv@gmail.com>
2021-12-12 15:59:32 +0400

    new commit

---
+++ 30112021/30Nov2021/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _30Nov2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topla(a,b) kimi
+Ferq(a,b)
+Hasil(a,b)
+Nisbet(a,b) 
+
+methodlarini hazirladiqdan sonra 
+
+Istifadecinin daxil etdiyi (Console.ReadLine) a ve b ededlerine gore bu methodlari cagirib istifade edib
+return deyeri ekrana cap edin */
+            
+            Console.WriteLine(" a reqemini daxil edin: ");
+           int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b reqemini daxil edin: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+           
+
+           
+            Console.WriteLine (Topla(num1, num2));
+         Console.WriteLine (Ferq(num1, num2));
+         Console.WriteLine (Hasil(num1, num2));
+          Console.WriteLine(Nisbet(num1, num2));
+
+
+
+        }
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Ferq(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Hasil(int a, int b)
+        {
+            return a * b;
+        }
+
+        static double Nisbet(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
