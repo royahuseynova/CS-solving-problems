468b6e7b1d94665723d16e750570ebd301bcf7c3
royahuseynova <ryhsynv@gmail.com>
2021-12-20 21:29:04 +0400

    Task 3

---
+++ Task3/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class CarClass
+    {
+        public string brand;
+        public string model;
+        public int productionDate;
+        public string color;
+        public double price;
+
+        public string GetCar()
+        {
+            return $"{brand}, {model}, {productionDate}, {color}, {price}";
+        }
+    }
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -17,6 +17,45 @@ Sonra 4 xususiyyeti olacaq sekilde bir Car classı yaradın
 son dersdeki kimi istifadeciden say saorushulsun ve o qeder sayda mashini massive elave etmek(butun gostericilerini) */
 
 
+            Console.Write("Nece eded masin axtarirsiniz? Masin sayi: ");
+            CarClass[] cars = new CarClass[Convert.ToInt32(Console.ReadLine())];
+
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                CarClass carsInstance = new CarClass();
+                cars[i] = carsInstance;
+
+                Console.Write("Masin markasi daxil et: ");
+                carsInstance.brand = Console.ReadLine();
+
+                Console.Write("Masin modeli daxil et: ");
+                carsInstance.model = Console.ReadLine();
+
+                Console.Write("Istehsal tarixi daxil et: ");
+                carsInstance.productionDate = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Masin rengi daxil et: ");
+                carsInstance.color = Console.ReadLine();
+
+                Console.Write("Qiymet daxil et: ");
+                carsInstance.price = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"Axtardiginiz {i + 1}ci masin budur: {carsInstance.GetCar()}");
+                Console.WriteLine("-------------------------------------------");
+            }
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Yuxarida axtardiginiz masin siyahisi bunlardir: ");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.GetCar());
+
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+
 
         }
     }
